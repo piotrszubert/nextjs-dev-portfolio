0ff34f3d7b235af0ca5f042c16ff0e056fc77fee
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-02-05 18:28:14 +0100

    skeleton

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<style>
+
+</style>
+  
+<script>
+    import '../app.css';
+    import { page } from '$app/stores';
+    import Footer from '../lib/Footer.svelte';
+</script>
+
+
+<div>
+
+    
+</div>npm run dev
