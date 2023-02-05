1d7e4191ed1bb69abad717b84c1c1cf52659b4a9
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-02-05 19:33:02 +0100

    Update Navbar.svelte

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,46 @@
+<style>
+    .navbar {
+        padding: 20px 120px;
+    }
+
+    .navbar-wrapper {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    /* .navbar-logo {} */
+
+    .navbar-links {
+        display: flex;
+        gap: 1rem;
+    } 
+
+    .navbar-links a {
+        text-decoration: none;
+        color: var(--white);
+    }
+</style>
+
+<div class="container navbar">
+    <div class="navbar-wrapper">
+        <div class="navbar-logo">
+            <img src="/Logo.svg" alt="" srcset="">
+        </div>
+        <div class="navbar-links">
+            <a href="#">Products</a>
+            <a href="#">Features</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+            
+            <div>
+                <a href="">Login</a>
+
+                <a class="btn" href="#">
+                    Register
+                </a>
+            </div>
+        </div>
+    </div>
+
+</div>
