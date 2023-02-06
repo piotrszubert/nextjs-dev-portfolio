ada119e3ea9605281e87eaeaeef4312c5673c068
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-02-06 18:07:27 +0100

    moved styles, removed theme dir

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,22 @@
+<style>
+    .footer {
+        background-color: var(--dark-bg);
+
+    }
+
+    .footer-links a {
+        text-decoration: none;
+        color: var(--white);
+    }
+
+    .footer-links a:hover {
+        text-decoration: none;
+        color: var(--white);
+        opacity: .777;
+    }
+
+</style>
+
+<div class="container footer">
+    saazaas
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,16 +3,22 @@
     import Footer from '../lib/Footer.svelte';
     import Theme from '../lib/Theme/Theme.svelte';
     import Navbar from '../lib/Navbar.svelte';
+    import "@fontsource/inter";
 </script>
+
+<style>:root {
+    --font-family: "Inter";
+}
+</style>
 <Theme/>
 
 <div class="dark-bg">
     <Navbar/>
 
-    <div class="container">
-        qsaasasassaa
-        AQAQ
+    <div class="container" style="min-height: 80vh;">
+
         <slot />
         
     </div>
+    <Footer/>
 </div>
