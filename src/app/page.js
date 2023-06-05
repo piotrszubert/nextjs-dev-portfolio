295297a7d15a4e5f53bd7d27a540f9ccaa3269b7
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-06-05 21:08:09 +0200

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
-import Image from 'next/image'
+import { Hero } from "@/components/Hero"
 
 export default function Home() {
   return (
-    <main>
-      home
-    </main>
+      <div className="fixed inset-0 flex justify-center sm:px-8">
+        <div className="flex w-full max-w-7xl lg:px-8 bg-zinc-50">
+          <div className="w-full dark:bg-zinc-900">
+            <Hero/>
+          </div>
+        </div>
+     </div>
   )
 }
