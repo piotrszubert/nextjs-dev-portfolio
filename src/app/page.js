df0de9b2f55e21a18fa69110016aad7f76075497
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-06-06 18:27:36 +0000

    fixes

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-import { Hero } from "@/components/Hero"
+import Hero from "@/components/Hero"
 
 export default function Home() {
   return (
       <div className="fixed inset-0 flex justify-center sm:px-8">
-        <div className="flex w-full max-w-7xl lg:px-8 bg-zinc-50">
-          <div className="w-full dark:bg-zinc-900">
+        <div className="flex w-full max-w-7xl">
+          <div className="w-full dark:bg-zinc-900 px-4 sm:px-8 lg:px-12">
             <Hero/>
           </div>
         </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,9 @@
 "use client";
 
-const Hero = () => {
+export default function Hero() {
     return(
         <h1 className="text-4xl font-bold text-zinc-800 dark:text-zinc-100 sm:text-5xl">
             Software I use, gadgets I love, and other things I recommend.
         </h1>
     );
 }
-
-export { Hero }
