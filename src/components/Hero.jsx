290df3f46048b6e7599ca86f6335aa432b3452d6
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-06-12 22:47:36 +0200

    project reset

--- src/components/Hero.jsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function Hero() {
-    return(
-        <h1 className="text-4xl font-bold text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software I use, gadgets I love, and other things I recommend.
-        </h1>
-    );
-}
