3d4f761ac6134642242a5cccab6b169f2467f30d
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-07-07 10:02:44 +0000

    mobile menu

--- components/header.tsx
+++ components/header.tsx
@@ -1,22 +1,27 @@
-import { Github, Twitter } from 'lucide-react';
+import { Github, Twitter, Menu, X } from 'lucide-react';
 import { Button } from '@/components/ui/button'
 import NavMenu from './navMenu';
 import { ThemeToggle } from './themeToggle';
+import { useState } from 'react';
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header className="w-full sticky top-0 border-b bg-background mb-3 py-4">
       <div className="flex justify-between items-center">
         <div className='flex-1'>
           <span className="font-semibold text-lg">
-            {/* Logo */}
             <ThemeToggle/>
           </span>
         </div>
-        <div className='flex-1 justify-center'>
+        <div className='flex-grow hidden md:block'>
           <NavMenu/>
         </div>
-        <div className="flex flex-1 justify-end">
+        <div className="hidden md:flex flex-1 justify-end">
           <Button variant="ghost" size="icon">
             <Github />
           </Button>
@@ -24,7 +29,25 @@ export default function Header() {
             <Twitter />
           </Button>
         </div>
+        <div className='md:hidden'>
+          <Button variant="ghost" size="icon" className='md:hidden' onClick={toggleMenu}>
+            {isOpen ? <X/> : <Menu />}
+          </Button>
+        </div>
       </div>
+      {isOpen && (
+        <div className="mt-2 flex flex-col items-center">
+          <NavMenu />
+          <div className="mt-2">
+            <Button variant="ghost" size="icon">
+              <Github />
+            </Button>
+            <Button variant="ghost" size="icon">
+              <Twitter />
+            </Button>
+          </div>
+        </div>
+      )}
     </header>
   )
 }
