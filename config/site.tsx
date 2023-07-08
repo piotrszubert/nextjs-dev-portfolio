3a1521aa483fada5f1e0d357ef4799dffc38f63e
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-07-08 09:35:31 +0000

    added config file

---
+++ config/site.tsx
@@ -0,0 +1,11 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Next.js",
+  description:
+    "Description.",
+  links: {
+    twitter: "https://twitter.com",
+    github: "https://github.com",
+  },
+}
