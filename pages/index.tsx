3f66ed5489c93469b0a9a33e2b2621abdb169f04
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-07-09 08:04:01 +0000

    wrapped hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,14 +11,16 @@ export default function Home() {
       <main
         className={`${inter.className} max-w-4xl mx-auto`}
       >
-      <PageHero
-        heading='Software developer, founder and investor.'
-        subheading="I'm [Your Name], a software developer and entrepreneur with a passion for innovation and investment. As the founder and CEO of [Your Company Name], I strive to create groundbreaking technologies and opportunities that drive the future of technology and empower individuals to achieve their financial goals"
-      >
-        <Button variant={'outline'}>
-          button
-        </Button>
-      </PageHero>
+      <div className="max-w-3xl">
+        <PageHero
+          heading='Software developer, founder and investor.'
+          subheading="I'm [Your Name], a software developer and entrepreneur with a passion for innovation and investment. As the founder and CEO of [Your Company Name], I strive to create groundbreaking technologies and opportunities that drive the future of technology and empower individuals to achieve their financial goals"
+        >
+          <Button variant={'outline'}>
+            button
+          </Button>
+        </PageHero>
+      </div>
       </main>
     </Layout>
   )
