4fa11c2c027138783bd5d9de62c3d1cab162651f
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-07-10 08:56:07 +0000

    layout prop type

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,7 +3,11 @@ import Header from './header';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Layout({children}) {
+type LayoutProps = {
+    children: React.ReactNode;
+};
+
+export default function Layout({children}: LayoutProps) {
     return(
         <div className='max-w-4xl mx-auto px-4 lg:px-2'>
             <Header/>
