e6a4cd2fc881897f02f3d516bd25b8063abe271b
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-07-10 10:30:25 +0000

    added footer component

---
+++ components/footer.tsx
@@ -0,0 +1,19 @@
+import NavMenu from "./navMenu";
+
+export default function Footer() {
+  return (
+    <footer className="border-t mt-6 py-10">
+        <div className="flex flex-wrap gap-y-6 gap-x-3 justify-between">
+            <div>
+                <NavMenu
+                    markActiveLink={false}
+                />
+            </div>
+            <div className="text-muted-foreground">
+                &copy; 2023 Blitz.
+                All rights reserved. 
+            </div>
+        </div>
+    </footer>
+  )
+}
