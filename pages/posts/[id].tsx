fcc53db728910f6d2fbc854905725c8e76c8f572
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-07-12 13:49:26 +0000

    paths to articles

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,31 @@
+import Layout from "@/components/layout";
+import { getAllPostIds, getPostData } from '../../lib/posts';
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds();
+    return {
+      paths,
+      fallback: false,
+    };
+}
+
+export async function getStaticProps({ params }: any) {
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export default function Post({ postData }: any) {
+  return(
+    <Layout>
+      <main>
+        {postData.title}
+        {postData.id}
+        {postData.date}
+      </main>
+    </Layout>
+  );
+}
