2d362eb9ded7ef74c049823fdc32c33003854668
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-07-14 07:03:52 +0000

    fix spacing

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import NavMenu from "./navMenu";
 
 export default function Footer() {
   return (
-    <footer className="border-t mt-6 py-10">
+    <footer className="border-t mt-16 py-10">
         <div className="flex flex-wrap gap-y-6 gap-x-3 justify-between">
             <div>
                 <NavMenu
