c16ff2ca0c4491dc40fb4d01bac44432e8717f75
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-07-18 06:46:29 +0000

    added buttons to blog posts list

--- components/postsList.tsx
+++ components/postsList.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { siteConfig } from '@/config/site';
 import { Separator } from '@/components/ui/separator';
 import Link from 'next/link';
-
+import { Share2, Heart } from 'lucide-react';
+import { Button } from '@/components/ui/button';
 const colorVariant = siteConfig.accentColor.colorVariant;
 const { accentTextClass, accentBorderClass } = siteConfig.accentColor.colorVariants[colorVariant];
 
@@ -18,12 +19,14 @@ type PostsListProps = {
 }
 
 export default function PostsList({ posts }: PostsListProps) {
+  
   return (
     <div className="space-y-6">
       {posts.map(({ id, date, heading, subheading }: PostType, index: number) => (
         <>
-          <div key={id} className='max-w-lg'>
+          <div key={id} className='flex gap-3 flex-wrap sm:flex-nowrap'>
             <Link
+              className='max-w-lg'
               href={`blog/posts/${id}`}
             >
               <div
@@ -40,6 +43,16 @@ export default function PostsList({ posts }: PostsListProps) {
                 </p>
               </div>
             </Link>
+            <div className='grid place-items-start ps-3 sm:ps-0 sm:place-items-center flex-1'>
+              <div className='flex gap-3'>
+                <Button variant="ghost" size="icon">
+                  <Share2/>
+                </Button>
+                <Button variant="ghost" size="icon">
+                  <Heart/>
+                </Button>
+              </div>
+            </div>
           </div>
           {index !== posts.length - 1 && (
             <div className="mt-6">
