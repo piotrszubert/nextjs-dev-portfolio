ec4b16e2dc84f3f055d03c7dde13e18346e65146
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-12-05 22:00:41 +0100

    migrated home without posts list

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/header"
 import "../styles/globals.css"
 import { ThemeProvider } from "@/components/themeProvider"
+import Footer from "@/components/footer"
 
 export const metadata = {
   title: 'Next.js',
@@ -21,9 +22,9 @@ export default function RootLayout({
         enableSystem
         >
           <Header />
-          <div className='px-4 lg:px-2'>
-
+          <div className='px-4 lg:px-2 max-w-4xl mx-auto'>
             {children}
+            <Footer />
           </div>
         </ThemeProvider>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,31 @@
+import PageHero from "@/components/pageHero";
+import { siteConfig  } from "@/config/site";
+import LinkList from "@/components/linksList";
+import PostsList from "@/components/postsList";
+import WorkCard from "@/components/workCard";
+
 export default function Home() {
     return(
         <main>
-            app router migration
+            <PageHero
+            heading={siteConfig.pages.home.heading}
+            subheading={siteConfig.pages.home.subheading}
+          >
+            <LinkList
+              nameVisible={false}
+              paddingClass='pb-3'
+              flexDirectionClass='flex-row'
+            />
+          </PageHero>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
+          <div>
+            {/* <PostsList /> */}
+          </div>  
+          <div className='space-y-6 md:ps-10'>
+            {/* <NewsletterCard/> */}
+            <WorkCard/>
+          </div>
+        </div>
         </main>
     );
 }
