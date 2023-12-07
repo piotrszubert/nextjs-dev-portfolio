4934d7ffe72ec2ba5fc817287bccaf085f60773b
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-12-07 13:21:39 +0000

    migrated about, added missing hero

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,8 +1,14 @@
 import { PostsList } from "@/components/postsList";
+import PageHero from "@/components/pageHero";
+import { siteConfig } from "@/config/site";
 
 export default function BlogPage() {
     return(
         <main>
+            <PageHero
+          heading={siteConfig.pages.blog.heading}
+          subheading={siteConfig.pages.blog.subheading}
+        />
             <PostsList />
         </main>
     )
