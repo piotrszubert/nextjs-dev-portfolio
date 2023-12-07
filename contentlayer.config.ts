e2f4a94e16aa80f6b2588de7e704f11dcdb5e58d
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-12-07 13:04:15 +0000

    fix post page added mdx support

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -2,7 +2,9 @@ import { defineDocumentType, makeSource } from 'contentlayer/source-files'
 
 export const Post = defineDocumentType(() => ({
   name: 'Post',
-  filePathPattern: `**/*.md`,
+  // filePathPattern: `**/*.md`,
+  filePathPattern: `**/*.mdx`,
+  contentType: 'mdx',
   fields: {
     title: { type: 'string', required: true },
     description: { type: 'string', required: true },
