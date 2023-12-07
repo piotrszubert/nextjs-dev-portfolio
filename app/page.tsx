62a36a161c3a8e2368be2459a05e6642a04b1f51
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-12-07 12:08:13 +0000

    updated home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,37 @@
 import PageHero from "@/components/pageHero";
 import { siteConfig  } from "@/config/site";
 import LinkList from "@/components/linksList";
-import PostsList from "@/components/postsList";
 import WorkCard from "@/components/workCard";
 import Link from "next/link";
-
-
 import { compareDesc, format, parseISO } from 'date-fns'
 import { allPosts, Post } from 'contentlayer/generated'
 
+const colorVariant = siteConfig.accentColor.colorVariant;
+const { accentTextClass } = siteConfig.accentColor.colorVariants[colorVariant];
+
+
 function PostCard(post: Post) {
   return (
-    <div className="mb-8">
+    <div className={`space-y-3 relative hover:cursor-pointer hover:bg-accent ${'hover:' + accentTextClass} rounded-xl p-3 -m-3`}>
       <h2 className="mb-1 text-xl">
-        <Link href={post.url} className="text-blue-700 hover:text-blue-900 dark:text-blue-400">
+        <Link href={post.url}>
           {post.title}
         </Link>
       </h2>
-      <time dateTime={post.date} className="mb-2 block text-xs text-gray-600">
+      <time dateTime={post.date} className="text-xs text-muted-foreground font-semibold">
         {format(parseISO(post.date), 'LLLL d, yyyy')}
       </time>
+      <p className="flex gap-3 text-muted-foreground text-sm">
+        {post.description}
+      </p>
     </div>
   )
 }
 
 
 export default function Home() {
-
-
     const posts = allPosts.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
 
-    
     return(
         <main>
             <PageHero
@@ -44,11 +45,10 @@ export default function Home() {
             />
           </PageHero>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
-          <div>
+          <div className="space-y-6">
             {posts.map((post, idx) => (
                 <PostCard key={idx} {...post} />
             ))}
-            {/* <PostsList /> */}
           </div>  
           <div className='space-y-6 md:ps-10'>
             {/* <NewsletterCard/> */}
